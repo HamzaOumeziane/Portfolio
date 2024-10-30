d63fbf26d7a0c8101d6e0e4c166d94f5b8f1e0f2
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-10-30 15:16:49 -0400

    Faire page À propos

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+const openTab = (numElem) => {
+    var tabLinks = document.getElementsByClassName('tab-links')
+    var tabContents = document.getElementsByClassName('tab-contents')
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const curseur = document.querySelector('.curseur-personnalise');
+    for (var i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove('active-link')
+    }
 
-        document.addEventListener('mousemove', (e) => {
-            curseur.style.top = e.pageY + 'px';
-            curseur.style.left = e.pageX + 'px';
-        });
-    });
+    for(var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove('active-content')
+    }
 
+
+    tabLinks[numElem].classList.add('active-link')
+    tabContents[numElem].classList.add('active-content')
+    
+
+   
+}
