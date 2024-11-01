18bdb79222cc869d81ffe50f9738838797de332f
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-11-01 13:20:33 -0400

    sections coming soon

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const openTab = (numElem) => {
     var tabLinks = document.getElementsByClassName('tab-links')
     var tabContents = document.getElementsByClassName('tab-contents')
@@ -13,7 +15,31 @@ const openTab = (numElem) => {
 
     tabLinks[numElem].classList.add('active-link')
     tabContents[numElem].classList.add('active-content')
-    
+}
 
-   
+const animerComingSoon = () => {
+    const textElements = document.getElementsByClassName("coming-soon");
+    let compteurPoints = 0; 
+
+    console.log(textElements);
+
+    setInterval(function () {
+        compteurPoints = (compteurPoints + 1) % 4;
+        for (let i = 0; i < textElements.length; i++) {
+            textElements[i].textContent = "Coming soon" + ".".repeat(compteurPoints);
+        }
+    }, 500);
+
+    
+}
+
+animerComingSoon();
+
+
+const viderCases = () => {
+    var cases = formulaire.getElementsByTagName('input');
+    
+    for (var i = 0; i < cases.length; i++) {
+        cases[i].value = '';
+    }
 }
